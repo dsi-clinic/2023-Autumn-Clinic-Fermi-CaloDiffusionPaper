e59fac2e5bda1f26e4a3cb16bd132741d3bd6b41
Kevin Lin <klin665@fe01.ds.uchicago.edu>
2024-10-17 17:41:00 -0500

    Fix if criteria for device selection

--- sbatch/new_repo_train.sh
+++ sbatch/new_repo_train.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #
 #SBATCH --job-name=fermi
-#SBATCH --output=/home/${USER}/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/logs/%j.%N.stdout
-#SBATCH --error=/home/${USER}/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/logs/%j.%N.stderr
-#SBATCH --chdir=/home/${USER}/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/scripts
+#SBATCH --output=/home/klin665/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/logs/%j.%N.stdout
+#SBATCH --error=/home/klin665/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/logs/%j.%N.stderr
+#SBATCH --chdir=/home/klin665/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/scripts
 #SBATCH --partition=general
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=128G
 #SBATCH --time=12:00:00
-source /home/${USER}/miniconda3/etc/profile.d/conda.sh
-conda activate fermi
 
 
 python train_diffu.py \
     --data_folder /net/projects/fermi-1/data/dataset_1 \
     --config /home/${USER}/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/configs/config_dataset1_photon.json \
-	--model "Diffu" 
+	--model "Diffu"
