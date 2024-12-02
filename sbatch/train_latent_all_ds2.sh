c58a17f4ee08d8e74c84bdcf0761719143e84fc9
Aaron E Zhang <aaronz@fe01.ds.uchicago.edu>
2024-12-02 12:43:35 -0600

    added ds2 training for ae in klin

--- sbatch/train_latent_all_ds2.sh
+++ sbatch/train_latent_all_ds2.sh
@@ -5,23 +5,22 @@
 #SBATCH --error=/home/aaronz/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/logs/%j.%N.stderr
 #SBATCH --chdir=/home/aaronz/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/
 #SBATCH --partition=general
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --mem=128G
 #SBATCH --time=12:00:00
 source /home/${USER}/.bashrc
 source activate fermi
 
-layer_size_unet=("16 16 16 16 16" "16 16 16 16" "16 16 16 32 32" "32 32 32" "64 64 64")
-epoch=400
-learning_rate=0.00004
+layer_size_unet=("16 16 16" "16 16 32 32" "32 32 32" "32 32 32 32")
+learning_rate="4e-4"
 
 for lsu in "${layer_size_unet[@]}"
 do
     str_lsu="${lsu//" "/"_"}"
     python3 scripts/train_diffu.py \
     --data_folder /net/projects/fermi-1/data/dataset_2 \
-    --config /home/${USER}/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/configs/config_dataset2.json \
-    --model_loc /net/projects/fermi-1/lijiaxin/ae_models/dataset2_AE/static_$str_lsu"e"$epoch"lr"$learning_rate/final.pth \
+    --config /home/aaronz/2023-Autumn-Clinic-Fermi-CaloDiffusionPaper/configs/config_dataset2.json \
+    --model_loc /net/projects/fermi-1/klin/dataset2_AE_$str_lsu"_"$learning_rate/final.pth \
     --layer_sizes  $lsu \
     --model "Latent_Diffu" \
 
